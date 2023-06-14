d88200424485a60fd05b3a22f3cda1c9598cf251
ThomasBarker220 <james.barker@duke.edu>
2023-06-13 23:05:55 -0400

    initial commit

---
+++ src/Form.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Form = ({ addColor }) => {
+  const [color, setColor] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
+
+  return (
+    <section className="container">
+      <h4>Color Generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#f15021"
+          onSubmit={handleSubmit}
+        />
+        <button className="btn" type="submit" style={{ background: color }}>
+          Generate
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
